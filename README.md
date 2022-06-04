c5a9bd770c208257582b670079837e10ca725a91
Lukáš Litvan <lukas.litvan@gmail.com>
2022-06-04 13:28:31 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sprinkled
+Simple plant manager created as a semestral work in subject NI-IOS at FIT CTU. 
---
+++ Sprinkled/SprinkledApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprinkledApp.swift
+//  Sprinkled
+//
+//  Created by Lukáš Litvan on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SprinkledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
